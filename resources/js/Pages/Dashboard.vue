d283804b0c78712dddaa08758711cc217e9cf940
johnvergilbulac93 <johnvergilbulac93@gmail.com>
2022-08-22 10:12:11 +0800

    changes in dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,45 +8,20 @@ const props = defineProps({
 </script>
 
 <template>
+
     <Head title="Dashboard" />
     <BreezeAuthenticatedLayout>
-        <div class="flex flex-wrap">
-            <Card
-                :image="`/images/supplier.png`"
-                :link="`/supplier`"
-                :count="supplier"
-                :title="`SUPPLIER`"
-            />
-            <Card
-                :image="`/images/customer.png`"
-                :link="`/customer`"
-                :count="customer"
-                :title="`CUSTOMER`"
-            />
-            <Card
-                :image="`/images/item.png`"
-                :link="`/item`"
-                :count="item"
-                :title="`ITEM`"
-            />
-            <Card
-                :image="`/images/bu.png`"
-                :link="`/business-unit`"
-                :count="business_unit"
-                :title="`BUSINESS UNIT`"
-            />
+        <div class="flex flex-wrap space-y-2">
+            <Card :image="`/images/supplier.png`" :link="`/supplier`" :count="supplier" :title="`SUPPLIER`" />
+            <Card :image="`/images/customer.png`" :link="`/customer`" :count="customer" :title="`CUSTOMER`" />
+            <Card :image="`/images/item.png`" :link="`/item`" :count="item" :title="`ITEM`" />
+            <Card :image="`/images/bu.png`" :link="`/business-unit`" :count="business_unit" :title="`BUSINESS UNIT`" />
         </div>
-        <div class="flex">
-            <Card2
-                :image="`/images/www.png`"
-                :link="`http://172.16.161.37/uploading-ar`"
-                :title="`DC Middleware`"
-            />
-            <Card2
-                :image="`/images/www.png`"
-                :link="`http://172.16.161.43/onlinetorrfs/index2`"
-                :title="`TOR and RFS`"
-            />
+        <div class="flex flex-wrap space-y-2">
+            <Card2 :image="`/images/www.png`" :link="`http://172.16.221.1/uploading`" :title="`UTILITY`" />
+            <Card2 :image="`/images/www.png`" :link="`http://172.16.161.37/uploading-ar`" :title="`DC Middleware`" />
+            <Card2 :image="`/images/www.png`" :link="`http://172.16.161.43/onlinetorrfs/index2`"
+                :title="`TOR and RFS`" />
         </div>
     </BreezeAuthenticatedLayout>
 </template>
